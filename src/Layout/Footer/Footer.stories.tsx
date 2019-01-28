f32ecd1aef8dae2c6d2aa11d93ff402fd4610103
Remington Reackhof <rreackhof@rmn.com>
2019-01-28 13:52:15 -0600

    Add custom footer, add links in default footer

--- src/Layout/Footer/Footer.stories.tsx
+++ src/Layout/Footer/Footer.stories.tsx
@@ -10,8 +10,27 @@ const StyledStory = styled.div`
   padding: 2rem 0;
 `;
 
-storiesOf('Components/Footer', module).add('default', () => (
-    <StyledStory>
-        <Footer>Story</Footer>
-    </StyledStory>
-));
+storiesOf('Components/Footer', module)
+    .add('default', () => (
+        <StyledStory>
+            <Footer/>
+        </StyledStory>
+    ))
+    .add('custom navigation', () => (
+        <StyledStory>
+            <Footer>
+                {Footer.renderLinks([
+                    {
+                        title: 'Find Deals',
+                        key: 1,
+                        children: [
+                            { label: 'Seasonal/Ideas', href: '#', key: 1 },
+                            { label: 'Browse Categories', href: '/coupons', key: 2 },
+                            { label: 'Exclusive Deals', href: '#', key: 3 },
+                            { label: 'Deal Squad', href: '#', key: 4 },
+                        ],
+                    }
+                ])}
+            </Footer>
+        </StyledStory>
+    ));
--- src/Layout/Footer/FooterSection/FooterSection.stories.tsx
+++ src/Layout/Footer/FooterSection/FooterSection.stories.tsx
@@ -12,6 +12,21 @@ const StyledStory = styled.div`
 
 storiesOf('Components/Footer/FooterSection', module).add('default', () => (
     <StyledStory>
-        <FooterSection />
+        <FooterSection title="Section Title">
+            <ul>
+                <li>
+                    <a href="#">Link 1</a>
+                </li>
+                <li>
+                    <a href="#">Link 2</a>
+                </li>
+                <li>
+                    <a href="#">Link 3</a>
+                </li>
+                <li>
+                    <a href="#">Link 4</a>
+                </li>
+            </ul>
+        </FooterSection>
     </StyledStory>
 ));
