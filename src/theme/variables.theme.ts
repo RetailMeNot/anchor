eaafb7f5eeb7e996038761f47d68694c5e9a6d3f
Steve Swanson <ss01736@rmn.com>
2019-02-06 15:54:03 -0600

    * removed the baseFontSize variable from variables.theme as it was no longer necessary
    * no longer importing any variables into sizes.theme as it is no longer necessary
    
    * all values in the Sizes objecct in sizes.theme have been converted to rem's
    
    * all calculated values using baseFontSize have been replaced with rem's

--- src/theme/variables.theme.ts
+++ src/theme/variables.theme.ts
@@ -1,7 +1,5 @@
-const baseFontSize = 16;
-
 export default {
   baseFontSize: 16,
   fontFamily: 'Avenir Next, Segoe UI, Helvetica, Roboto, sans-serif',
-  borderRadius: `${baseFontSize / 4}px`,
+  borderRadius: '0.25rem',
 };
