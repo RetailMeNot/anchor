6acc831f18a4b63e0d8c6998d1906aed82a54765
Steve Swanson <ss01736@rmn.com>
2019-04-01 16:48:10 -0500

    * rewrote the Page component to allow users to select one of two layouts (for now)

--- src/Layout/Page/Page.component.tsx
+++ src/Layout/Page/Page.component.tsx
@@ -2,12 +2,10 @@
 import * as React from 'react';
 // VENDOR
 import styled from 'styled-components';
-// import { Box } from '@rebass/grid';
-// STYLES
-import { colors, sizes } from '../../theme';
+import { Grid, Cell } from 'styled-css-grid';
 // COMPONENTS
-import { Footer, Header } from '../';
-import { NormalizeCSS, GlobalCSS } from '../../theme/GlobalStyles';
+import { NormalizeCSS } from '../../theme/GlobalStyles';
+import { CustomAdaptor, StandardBreakpoints } from '../../Grid/Adaptor';
 
 const StyledPageElement = styled.section`
     display: flex;
@@ -26,25 +24,95 @@ const StyledPageElement = styled.section`
     }
 `;
 
-const StyledBodyElement = styled.section`
-    flex: 1 0 auto;
-    background-color: ${colors.silver.base};
-    max-width: 100vw;
-    align-self: stretch;
-    padding: ${sizes.padding.lg} ${sizes.padding.xl};
-`;
+// const StyledBodyElement = styled.section`
+//     flex: 1 0 auto;
+//     background-color: ${colors.silver.base};
+//     max-width: 100vw;
+//     align-self: stretch;
+//     padding: ${sizes.padding.lg} ${sizes.padding.xl};
+// `;
 
 interface PageProps {
+    layout?: string;
+    header?: any;
+    footer?: any;
+    sidebarWidth?: string | number;
+    headerHeight?: string | number;
     className?: string;
     children?: any;
 }
 
-export const Page = ({ children }: PageProps): React.ReactElement<any> => (
+// ------------------------------------------------------------------------------------------------------------------
+
+interface DefaultLayoutProps {
+    header?: any;
+    footer?: any;
+    headerHeight?: string | number;
+    className?: string;
+    children?: any;
+}
+
+const DefaultLayout = (props: DefaultLayoutProps): React.ReactElement<any> => (
+    <div>
+        {props.header && props.header}
+        {props.children}
+        {props.footer && props.footer}
+    </div>
+);
+
+// ------------------------------------------------------------------------------------------------------------------
+
+interface SidebarLayoutProps {
+    header?: any;
+    footer?: any;
+    sidebarWidth?: string | number;
+    headerHeight?: string | number;
+    className?: string;
+    children?: any;
+}
+
+const SidebarLayout = (props: SidebarLayoutProps): React.ReactElement<any> => (
+    <Grid
+        columns={props.sidebarWidth ? `${props.sidebarWidth} 1fr` : "13.75rem 1fr"}
+        rows={props.headerHeight ? `minmax(${props.headerHeight},auto) 1fr` : "minmax(7.5rem,auto) 1fr"}
+        height={"100vh"}
+    >
+
+        {props.header &&
+            <Cell width={2}>
+                {props.header}
+            </Cell>
+        }
+
+        <CustomAdaptor maxWidth={StandardBreakpoints.sm.max}>
+            <Cell width={2}>{props.children}</Cell>
+        </CustomAdaptor>
+
+        <CustomAdaptor minWidth={StandardBreakpoints.md.min}>
+            <Cell>Menu</Cell>
+            <Cell>{props.children}</Cell>
+        </CustomAdaptor>
+
+        {props.footer &&
+            <Cell width={2}>
+                {props.footer}
+            </Cell>
+        }
+    </Grid>
+);
+
+// ------------------------------------------------------------------------------------------------------------------
+
+export const Page = (props: PageProps): React.ReactElement<any> => (
     <StyledPageElement>
         <NormalizeCSS />
-        <GlobalCSS />
-        <Header />
-        <StyledBodyElement>{children}</StyledBodyElement>
-        <Footer />
+
+        {(() => {
+            switch (props.layout) {
+                case 'sidebar': return <SidebarLayout {...props} />;
+                default: return <DefaultLayout {...props} />;
+            }
+        })()}
+
     </StyledPageElement>
 );
