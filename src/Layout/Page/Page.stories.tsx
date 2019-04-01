16001e723e3e3adc7ca7dbdcad4584ea6fab94f4
Steve Swanson <ss01736@rmn.com>
2019-04-01 16:48:10 -0500

    * rewrote the Page component to allow users to select one of two layouts (for now)

--- src/Layout/Page/Page.stories.tsx
+++ src/Layout/Page/Page.stories.tsx
@@ -5,11 +5,21 @@ import { storiesOf } from '@storybook/react';
 import styled from 'styled-components';
 // COMPONENT
 import { Page } from './Page.component';
+import { Footer } from '../Footer/Footer.component';
+import { Header } from '../Header/Header.component';
 
 const StyledStory = styled.div``;
 
 storiesOf('Components/Layout/Page', module).add('default', () => (
     <StyledStory>
-        <Page>Story</Page>
+        <Page
+            layout="sidebar"
+            sidebarWidth="100px"
+            headerHeight="50px"
+            header={<Header />}
+            footer={<Footer />}
+        >
+            <h1>Hello!</h1>
+        </Page>
     </StyledStory>
 ));
