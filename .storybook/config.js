d5f2b798d7535537110c555032acab860389a96e
Steve Swanson <ss01736@rmn.com>
2019-04-02 17:52:15 -0500

    * major rework. Page only accepts header/footer props, now there are Layout components that work with Page.
    * enabled the withInfo addOn for Storybook
    
    * lots of comments & code cleanup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,14 @@
 // STORYBOOK
 import { configure, addDecorator } from '@storybook/react';
 // ADDONS
+import { withInfo } from '@storybook/addon-info';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withViewport } from '@storybook/addon-viewport';
 
+addDecorator(withInfo({
+  source:false,
+}));
 addDecorator(withA11y);
 addDecorator(withKnobs);
 addDecorator(withViewport);
