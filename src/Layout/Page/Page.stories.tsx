1a0509ff498f947df3179b33546decab2e7e241a
Steve Swanson <ss01736@rmn.com>
2019-04-03 14:22:36 -0500

    * added in the ability for the user to pick background color for the layout, and the content within the layout
    * added the ability to pick the width of the content within the layout

--- src/Layout/Page/Page.stories.tsx
+++ src/Layout/Page/Page.stories.tsx
@@ -3,12 +3,21 @@ import * as React from 'react';
 // STORYBOOK
 import { storiesOf } from '@storybook/react';
 import styled from 'styled-components';
-import { select, text } from '@storybook/addon-knobs';
+import { select, text, color } from '@storybook/addon-knobs';
 // COMPONENT
-import { Page, SidebarLayout, DefaultLayout, DEFAULT_LAYOUT_WIDTH, DEFAULT_SIDEBAR_WIDTH } from './Page.component';
+import {
+  Page,
+  SidebarLayout,
+  DefaultLayout,
+  DEFAULT_LAYOUT_WIDTH,
+  DEFAULT_CONTENT_WIDTH,
+  DEFAULT_SIDEBAR_WIDTH,
+  TRANSPARENT,
+} from './Page.component';
 import { Footer } from '../Footer/Footer.component';
 import { Header } from '../Header/Header.component';
 import { Sider } from '../Sider/Sider.component';
+import { colors } from '../../theme';
 
 // Constants
 // ------------------------------------------------------------------------------------------------------------------
@@ -19,11 +28,13 @@ const alignmentOptions = {
 
 // Styled Components
 // ------------------------------------------------------------------------------------------------------------------
-const StyledStory = styled.div``;
+const StyledStory = styled.div`
+  background: ${colors.white.base};
+`;
 
 // Stories
 // ------------------------------------------------------------------------------------------------------------------
-storiesOf('Components/Layout/Page', module).add('default', () => (
+storiesOf('Components/Layout/Page', module).add('Default Layout', () => (
   <StyledStory>
     <Page
       header={<Header />}
@@ -31,6 +42,9 @@ storiesOf('Components/Layout/Page', module).add('default', () => (
     >
       <DefaultLayout
         layoutWidth={text('Layout Width', DEFAULT_LAYOUT_WIDTH)}
+        contentWidth={text('Content Width', DEFAULT_CONTENT_WIDTH)}
+        layoutBackgroundColor={color('Layout Background Color', TRANSPARENT)}
+        contentBackgroundColor={color('Content Background Color', TRANSPARENT)}
       >
         <h1>Hello!</h1>
       </DefaultLayout>
@@ -38,16 +52,19 @@ storiesOf('Components/Layout/Page', module).add('default', () => (
   </StyledStory>
 ));
 
-storiesOf('Components/Layout/Page', module).add('sidebar', () => (
+storiesOf('Components/Layout/Page', module).add('Sidebar Layout', () => (
   <StyledStory>
     <Page
       header={<Header />}
       footer={<Footer />}
     >
       <SidebarLayout
+        layoutWidth={text('Layout Width', DEFAULT_LAYOUT_WIDTH)}
+        contentWidth={text('Content Width', DEFAULT_CONTENT_WIDTH)}
+        layoutBackgroundColor={color('Layout Background Color', TRANSPARENT)}
+        contentBackgroundColor={color('Content Background Color', TRANSPARENT)}
         sidebar={<Sider />}
         sidebarAlign={select('Sidebar Alignment', alignmentOptions, 'left')}
-        layoutWidth={text('Layout Width', DEFAULT_LAYOUT_WIDTH)}
         sidebarWidth={text('Sidebar Width', DEFAULT_SIDEBAR_WIDTH)}
       >
         <h1>Hello!</h1>
