1649f62f85e8352478392338309595ab7daf26a9
Kameron <kameron.sween@rmn.com>
2019-06-26 13:07:02 -0500

    chore(theme): added todo for readme integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
+// TODO: Add this back when the library supports the newest storybook build
 // import { addReadme } from 'storybook-readme';
 
 addDecorator(withInfo({
