147e8587471454e547d46a4644e61ae44d8020fa
Steve Swanson <ss01736@rmn.com>
2019-06-28 12:50:07 -0500

    feat: New HOC, AddLocation. Gives any component access to the router's location prop
    fix: Using ^, no longer have to use window.location which was making the build fail
    
    fix: updated all paths so that activeClassName correctly is fired for sidenav
    
    chore: Moved logic from constructor into componentDidMount
    
    chore: prettier updates
    
    chore: Abbreviated the check for if there is a pattern
    
    fix: Changed the prop name of theme to variant for CollapseGroup

---
+++ docs/src/components/Navigation/AddLocation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Location } from '@reach/router';
+
+// Gatsby now uses @reach/router which makes it so that not every component has access to the location
+// prop like it used to in Gatsby 1.x. This HOC adds that prop to the component passed to it.
+export function AddLocation(Component: any) {
+    return class EnhancedWithLocation extends React.Component {
+        render() {
+            return(
+                <Location>
+                    {locationProps => <Component {...locationProps} {...this.props} />}
+                </Location>
+            );
+        }
+    };
+}
