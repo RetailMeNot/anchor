2210a4266e51c8bde1d0509bca33c43d54bdb02d
Kameron <kameron.sween@rmn.com>
2019-08-08 12:18:08 -0500

    chore(checkbox): tweaking component API

--- src/Form/Checkbox/Checkbox.component.tsx
+++ src/Form/Checkbox/Checkbox.component.tsx
@@ -4,19 +4,10 @@ const { forwardRef } = React;
 
 // VENDOR
 import styled, { css } from '@xstyled/styled-components';
-import { compose } from '@xstyled/system';
 import classnames from 'classnames';
 
 // ANCHOR
 import { Check } from '../../Icon';
-import {
-    space as spaceStyles,
-    SpaceProps,
-    color as colorStyles,
-    ColorProps,
-    backgroundColor as backgroundColorStyles,
-    BackgroundColorProps,
-} from '@xstyled/system';
 
 const HiddenInput = styled('input')<CheckboxProps>`
     visibility: hidden;
@@ -41,12 +32,6 @@ const StyledCheckbox = styled('label')<CheckboxProps>`
     // todo: may want to read from a theme value here
     border-radius: 2px;
 
-    ${compose(
-        spaceStyles,
-        colorStyles,
-        backgroundColorStyles
-    )}
-
     ${({ size, disabled }) =>
         css({
             height: size,
@@ -56,10 +41,11 @@ const StyledCheckbox = styled('label')<CheckboxProps>`
         })}
 `;
 
-interface CheckboxProps extends SpaceProps, ColorProps, BackgroundColorProps {
+interface CheckboxProps {
     className?: string;
     id?: string;
     htmlFor?: string;
+    backgroundColor?: string;
 
     checked?: boolean;
     disabled?: boolean;
