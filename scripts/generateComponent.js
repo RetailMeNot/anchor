adb2ad62bea7a3a3a63f5ae068196aa39fbfe780
Brandon Lilly <brandonjlilly@gmail.com>
2019-09-11 16:27:07 -0500

    feat(generate): allow subfolder components, check if already existing

--- scripts/generateComponent.js
+++ scripts/generateComponent.js
@@ -1,45 +1,56 @@
-// e.g. yarn generate Foo
+// e.g.
+//   yarn generate Foo
+//   yarn generate Form/Foo
 
 const fs = require('fs-extra');
 const argv = require('yargs').argv;
 const replace = require('replace-in-file');
 const Renamer = require('renamer');
 const renamer = new Renamer();
+const chalk = require('chalk');
 
-const componentName = argv._[0];
+try {
+    const componentPath = argv._[0];
 
-if (componentName) {
+    if (!componentPath) {
+        throw new Error(
+            'No component name defined, e.g. yarn generate MyComponent'
+        );
+    }
+
+    const componentName = componentPath.split('/').pop();
+
     const srcPath = 'scripts/MyComponent';
-    const destPath = `src/${componentName}`;
+    const destPath = `src/${componentPath}`;
     const files = [`${destPath}/*`, `${destPath}/**/*`];
 
-    try {
-        // copy template component
-        fs.copySync(srcPath, destPath);
-
-        // replace text in files
-        replace.sync({
-            files,
-            from: /MyComponent/g,
-            to: componentName,
-        });
-        replace.sync({
-            files,
-            from: /mycomponent/g,
-            to: componentName.toLowerCase(),
-        });
-
-        // rename files
-        renamer.rename({
-            files: [`${destPath}/*`],
-            find: 'MyComponent',
-            replace: componentName,
-        });
-
-        console.log(`Generated new component: ${componentName}`);
-    } catch (error) {
-        console.error('Error occurred:', error);
+    if (fs.existsSync(destPath)) {
+        throw new Error('Component path already exists');
     }
-} else {
-    console.error('No component name defined, e.g. yarn generate MyComponent');
+
+    fs.copySync(srcPath, destPath);
+
+    // replace text in files
+    replace.sync({
+        files,
+        from: /MyComponent/g,
+        to: componentName,
+    });
+    replace.sync({
+        files,
+        from: /mycomponent/g,
+        to: componentName.toLowerCase(),
+    });
+
+    // rename files
+    renamer.rename({
+        files: [`${destPath}/*`],
+        find: 'MyComponent',
+        replace: componentName,
+    });
+
+    console.log('✅  Generated new component:', chalk.bold(componentPath));
+} catch (error) {
+    console.error(chalk.red(error));
+    process.exitCode = 1;
 }
